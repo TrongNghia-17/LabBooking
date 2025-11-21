1454a23ea294109f0b84a7e59e2cd62588832fde
MinhPhuc <phucvahau157359@gmail.com>
2025-11-21 22:57:52 +0700

    Update booking entity

---
+++ src/LabBooking.Infrastructure/Persistence/Migrations/20251121155712_add_PendingSlotsJson_toBooking.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabBooking.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_PendingSlotsJson_toBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PendingSlotsJson",
+                table: "Bookings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PendingSlotsJson",
+                table: "Bookings");
+        }
+    }
+}
