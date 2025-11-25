0f03b5692ba65a9b020731b92131fe511dd47a10
MinhPhuc <phucvahau157359@gmail.com>
2025-11-25 13:11:47 +0700

    Get Booking History

---
+++ src/LabBooking.Application/Features/HistoryBooking/Queries/GetMyBookingHistory/GetMyBookingHistoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using LabBooking.Application.Features.Booking.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabBooking.Application.Features.HistoryBooking.Queries.GetMyBookingHistory
+{
+    public record GetMyBookingHistoryQuery(Guid UserId) : IRequest<List<BookingResponse>>;
+}
---
+++ src/LabBooking.Application/Features/HistoryBooking/Queries/GetMyBookingHistory/GetMyBookingHistoryQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using LabBooking.Application.Features.Booking.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabBooking.Application.Features.HistoryBooking.Queries.GetMyBookingHistory
+{
+    public class GetMyBookingHistoryQueryHandler(
+    IBookingRepository bookingRepository,
+    IMapper mapper
+    ) : IRequestHandler<GetMyBookingHistoryQuery, List<BookingResponse>>
+    {
+        public async Task<List<BookingResponse>> Handle(GetMyBookingHistoryQuery request, CancellationToken token)
+        {
+            var bookings = await bookingRepository.GetHistoryByUserIdAsync(request.UserId);
+            var bookingResponses = mapper.Map<List<BookingResponse>>(bookings);
+            return bookingResponses;
+        }
+    }
+}
