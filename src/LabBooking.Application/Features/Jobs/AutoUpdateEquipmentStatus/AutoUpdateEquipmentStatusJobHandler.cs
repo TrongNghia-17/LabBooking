e8bcbbf4d618b4c2d6b4bdab738c10cab0b7a470
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2025-11-28 21:00:05 +0700

    done RunScanJob, fix create equipmaintain

---
+++ src/LabBooking.Application/Features/Jobs/AutoUpdateEquipmentStatus/AutoUpdateEquipmentStatusJobHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace LabBooking.Application.Features.Jobs.AutoUpdateEquipmentStatus;
+
+public class AutoUpdateEquipmentStatusJobHandler(
+    IEquipmentMaintainScheduleRepository repository,
+    ILogger<AutoUpdateEquipmentStatusJobHandler> logger
+    ) : IRequestHandler<AutoUpdateEquipmentStatusJobCommand, string>
+{
+    public async Task<string> Handle(AutoUpdateEquipmentStatusJobCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("CronJob: Bắt đầu quét trạng thái thiết bị...");
+
+        var resultMessage = await repository.ProcessAutoStatusUpdatesAsync(cancellationToken);
+
+        logger.LogInformation(message: resultMessage);
+
+        return resultMessage;
+    }
+}
