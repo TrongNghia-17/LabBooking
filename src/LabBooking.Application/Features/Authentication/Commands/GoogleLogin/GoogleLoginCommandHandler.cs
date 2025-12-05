203e6530e7dae1805c6bd53d18f9e1fa455378c0
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2025-12-05 14:04:00 +0700

    fix add role google

--- src/LabBooking.Application/Features/Authentication/Commands/GoogleLogin/GoogleLoginCommandHandler.cs
+++ src/LabBooking.Application/Features/Authentication/Commands/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -29,9 +29,9 @@ public class GoogleLoginCommandHandler(
         string userRole;
         if (googlePayload.Email.EndsWith("@fpt.edu.vn", StringComparison.OrdinalIgnoreCase))
             userRole = "Student";
-        else if (googlePayload.Email.EndsWith("@fe.edu.vn", StringComparison.OrdinalIgnoreCase))
+        if (googlePayload.Email.EndsWith("@fe.edu.vn", StringComparison.OrdinalIgnoreCase))
             userRole = "Lecturer";
-        else if (googlePayload.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
+        if (googlePayload.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
             userRole = "Manager";
         else
             throw new ForbidException("Email domain is not allowed.");
