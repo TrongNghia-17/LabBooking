d548fb587d7ee9bfbef0353b51486908b8be2f0e
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2025-12-05 15:17:09 +0700

    fix google-login if else

--- src/LabBooking.Application/Features/Authentication/Commands/GoogleLogin/GoogleLoginCommandHandler.cs
+++ src/LabBooking.Application/Features/Authentication/Commands/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -29,9 +29,9 @@ public class GoogleLoginCommandHandler(
         string userRole;
         if (googlePayload.Email.EndsWith("@fpt.edu.vn", StringComparison.OrdinalIgnoreCase))
             userRole = "Student";
-        if (googlePayload.Email.EndsWith("@fe.edu.vn", StringComparison.OrdinalIgnoreCase))
+        else if (googlePayload.Email.EndsWith("@fe.edu.vn", StringComparison.OrdinalIgnoreCase))
             userRole = "Lecturer";
-        if (googlePayload.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
+        else if (googlePayload.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
             userRole = "Manager";
         else
             throw new ForbidException("Email domain is not allowed.");
