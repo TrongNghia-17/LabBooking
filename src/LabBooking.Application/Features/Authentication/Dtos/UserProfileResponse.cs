a7c1282423758317fb45b9ac0e24c6da2dfbc5f0
Huynh Trong Nghia <huynhtrongnghia15n@gmail.com>
2025-12-18 16:33:43 +0700

    fix: add numberphone into getprofie and edit fullname of getmanagerlabdetails method

--- src/LabBooking.Application/Features/Authentication/Dtos/UserProfileResponse.cs
+++ src/LabBooking.Application/Features/Authentication/Dtos/UserProfileResponse.cs
@@ -8,6 +8,7 @@ public record UserProfileResponse
     public Guid Id { get; init; }
     public string? Email { get; init; }
     public string? UserName { get; init; }
+    public string? PhoneNumber { get; init; }
     public string? AvatarUrl { get; init; }
     public IList<string> Roles { get; init; } = [];
 }
